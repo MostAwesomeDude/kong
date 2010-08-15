85d047ccd5ee4cfddbf93564b0319fcda278b579
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-08-15 06:01:41 -0700

    Add long_description to setup.py, from CHANGELOG and README.

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@
 
 from distutils.core import setup
 
+f = open("README.rst")
+long_description = f.read()
+f.close()
+f = open("CHANGELOG.rst")
+long_description += f.read()
+f.close()
+
 setup(
     name="kong",
     version="0.1",
     description="Kongregate-related utilities",
+    long_description=long_description,
     author="Corbin Simpson",
     author_email="MostAwesomeDude@gmail.com",
     url="http://github.com/MostAwesomeDude/kong",
