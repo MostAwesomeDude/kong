fb7f52dc5d36aa027b905ff690d5b3c7a7547cee
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-08-15 05:04:40 -0700

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="kong",
+    version="0.1",
+    description="Kongregate-related utilities",
+    author="Corbin Simpson",
+    author_email="MostAwesomeDude@gmail.com",
+    url="http://github.com/MostAwesomeDude/kong",
+    py_modules=["kong"],
+)
